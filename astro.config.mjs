92e25711e22ba7c82ef19c4bf37328c75abd99c0
Rafael Garcia <fafagm99@gmail.com>
2025-12-28 00:23:28 -0700

    Fix: add trailing slash to base URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://unmemero.github.io',
-  base: '/resume_page',
+  base: '/resume_page/',
   vite: {
     plugins: [tailwindcss()]
   },
