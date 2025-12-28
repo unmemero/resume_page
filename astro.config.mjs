7a89c6a56e742b77d3a62cbf7e29345449067e1f
Rafael Garcia <fafagm99@gmail.com>
2025-12-28 00:16:17 -0700

    unmemero :v

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://unmemero.github.io',
+  base: '/resume_page',
   vite: {
     plugins: [tailwindcss()]
   },
